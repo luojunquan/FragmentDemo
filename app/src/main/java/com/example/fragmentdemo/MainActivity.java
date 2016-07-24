4187a3622e39bc42235b118caab72b0ad5907e04
luojunquan.github.io <ljq906416@gmail.com>
2016-07-24 09:00:28 +0800

    关于如何添加Fragment的一个简单Demo

---
+++ app/src/main/java/com/example/fragmentdemo/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.fragmentdemo;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.Display;
+public class MainActivity extends FragmentActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Display display = getWindowManager().getDefaultDisplay();
+        if (display.getWidth() > display.getHeight()) {
+            Fragment1 fragment1 = new Fragment1();
+            getSupportFragmentManager().beginTransaction().replace(R.id.main_layout, fragment1).commit();
+        } else {
+            Fragment2 fragment2 = new Fragment2();
+            getSupportFragmentManager().beginTransaction().replace(R.id.main_layout, fragment2).commit();
+        }
+    }
+}
